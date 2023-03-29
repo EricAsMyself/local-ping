706e97ca7a02fcf63efcb357749a1fde28fc1c6b
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-03-28 20:40:33 -0600

    INITIAL COMMIT

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # local-ping
 pings local ip address and shows on a 4 digit 7 segment display
+
+#
---
+++ main.py
@@ -0,0 +1,110 @@
+import os
+# typo error in import
+import subprocess
+import speedtest  
+import time
+
+
+
+  
+st = speedtest.Speedtest()
+
+def num_and_letter(num, char):
+    print('hello')
+    
+
+def download_format (num):
+    if num < 10000000:
+        print(str(round((num/100000),2)) + "Kb/s")
+    elif num < 10000000000:
+        print(str(round((num/1000000),2)) + "Mb/s")
+    elif num < 10000000000000:
+        print(str(round((num/1000000000),2)) + "Gb/s")
+
+def display(str_disp):
+    
+    old_disp = str_disp
+    while True:
+        # length = 4
+        str_disp = old_disp
+        for x in range(len(str_disp) + 4):
+            print(str_disp[:4])
+            time.sleep(0.2)
+            str_disp =  str_disp[1:]
+            str_disp += " "
+        
+
+
+
+def setcode():
+    to_alphabet = {
+        "A": "ABFCEG",
+        "B": "ABCDEFG",
+        "C": "ADEF",
+        "D": "ABCDEF",
+        "E": "ADEFG",
+        "F": "ABEFG",
+        "G": "ABCDFG",
+        "H": "BCEFGH",
+        "I": "EF",
+        "J": "BCDE",
+        "K": "BCEFG",
+        "L": "DEF",
+        "M": "ABCEF",
+        "N": "ABCEF",
+        "O": "ABCDEF",
+        "P": "ABEFG",
+        "Q": "ABCDEFH",
+        "R": "ABCEFGH",
+        "S": "ACDFG",
+        "T": "EFG",
+        "U": "BCDEF",
+        "V": "BCDEF",
+        "W": "BCDEF",
+        "X": "BCDEF",
+        "Y": "FBGC",
+        "Z": "ACDFG",
+        "1": "EF",
+        "2": "ABDEG",
+        "3": "ABCDG",
+        "4": "BCFG",
+        "5": "ACDFG",
+        "6": "ACDEFG",
+        "7": "ABC",
+        "8": "ABCDEFG",
+        "9": "ABCFG",
+        "0": "ABCDEF",
+        ".": "H"
+        }
+    
+    ip_addresses = {
+        "192.168.1.10": "TRUENAS",
+        "192.168.1.11": "UNIFI",
+        "192.168.1.23": "HOMEBRIDGE"
+    }
+
+    connect = {}
+    disconnected = {}
+    
+    # for ping in range(1,10):
+    for address in ip_addresses:
+        res = subprocess.call(['ping', '-c', '3', address])
+        if res == 0:
+            connect[address] = ip_addresses[address]
+        elif res == 2:
+            disconnected[address] = ip_addresses[address]
+        else:
+            print("ping to", address, "failed!")
+    for x in connect:
+        print('connected '  + connect[x])
+    for x in disconnected:
+        print('connected ' + connect[x])
+    down = st.download()
+    download_format(down)
+    
+
+
+if __name__ == "__main__":
+    print("starting")
+    # setcode()
+    display('Here is the new one that I found.')
